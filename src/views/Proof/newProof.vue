852bf020259ec2e1e48c62b9c3318075ed1de7ba
whj998013 <whj998013@hotmail.com>
2019-01-10 12:21:51 +0800

    aaa

--- src/views/Proof/newProof.vue
+++ src/views/Proof/newProof.vue
@@ -1,25 +1,50 @@
 <style scoped>
 </style>
 <template>
-  <div>    
-    新的打样  
-  </div>                       
+  <div>
+    <div>
+      <Row
+        type="flex"
+        justify="space-between"
+      >
+        <Col span='16'>
+        </Col>
+        <Col>
+        <Button
+          size="large"
+          type="primary"
+          icon="md-checkmark"
+          @click="saveSample"
+        >&emsp;保存&emsp;</Button>
+        <Button
+          size="large"
+          type="warning"
+          icon="md-close"
+          @click="goBack"
+        >&emsp;取消&emsp;</Button>
+        </Col>
+      </Row>
+
+      <Form
+        ref='sample'
+        :label-width="75"
+        label-position="right"
+      >
+
+      </Form>
+    </div>
+  </div>
 </template>
 
 <script>
-
 //import bus from "../bus.js";
-export default {  
+export default {
   data: function() {
-    return {
- 
-    };
+    return {};
   },
-  methods: {
-    
-  },
+  methods: {},
   mounted: function() {
-       this.$bus.$emit('changeMenuItem',['打样管理','打样申请']);
+    this.$bus.$emit("changeMenuItem", ["打样管理", "打样申请"]);
   }
 };
 </script>
